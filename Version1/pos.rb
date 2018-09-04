1c5a8314f3ffeb536e0639bc2502e4896b70236f
Santosh2167 <santosh.poudyal@gmail.com>
2018-09-03 17:17:38 -0700

    working on items

--- Version1/pos.rb
+++ Version1/pos.rb
@@ -2,53 +2,76 @@ require 'yaml'
 
 class Item
 	ITEMS_FILE = "items.yml"
-
-	def initialize
+	
+	def initialize 
 		@items ||= YAML.load(File.read(ITEMS_FILE))
 	end
 
-	def create_item
-		item_key = ""
-		item = {}
-
-		p "Enter Product Key"
-		item_key = gets.chomp
-		
-		p "Enter Product name"
-		item["name"] = gets.chomp
-		
-		p "Enter Product price"
-		item["price"] = gets.chomp
-		
-		p "Enter Product margin"
-		item["margin"] = gets.chomp
-		
-		p "Enter Product quantity_in_hand"
-		item["quantity_in_hand"] = gets.chomp
-
-		file = File.open(ITEMS_FILE, "a")
-		file.write({item_key.to_sym => item}.to_yaml.sub("---",""))
-		file.close
-	end
-
-	def look_item(item_key)
-		@items[item_key.to_sym]
-	end
-
-	def list_all_items
-		@items.each do |key,i|
-			p key
-			p i["name"]
-			p i["price"]
-			p i["margin"]
-			p i["quantity_in_hand"]
-			p ""
-		end
+	def add_item
+		puts "in add_item method"
 	end
 
 end
 
-i = Item.new
-i.list_all_items
-i.look_item("tp")
-i.create_item
+
+
+clear_screen = %x{clear}
+puts clear_screen
+puts "lets Start"
+item = Item.new
+puts "calling the add_item"
+item.add_item
+
+
+# class Item
+# 	ITEMS_FILE = "items.yml"
+
+# 	def initialize
+# 		@items ||= YAML.load(File.read(ITEMS_FILE))
+# 	end
+
+# 	def create_item
+# 		item_key = ""
+# 		item = {}
+
+# 		p "Enter Product Key"
+# 		item_key = gets.chomp
+		
+# 		p "Enter Product name"
+# 		item["name"] = gets.chomp
+		
+# 		p "Enter Product price"
+# 		item["price"] = gets.chomp
+		
+# 		p "Enter Product margin"
+# 		item["margin"] = gets.chomp
+		
+# 		p "Enter Product quantity_in_hand"
+# 		item["quantity_in_hand"] = gets.chomp
+
+# 		file = File.open(ITEMS_FILE, "a")
+# 		file.write({item_key.to_sym => item}.to_yaml.sub("---",""))
+# 		file.close
+# 	end
+
+# 	def look_item(item_key)
+# 		@items[item_key.to_sym]
+# 	end
+
+# 	def list_all_items
+# 		@items.each do |key,i|
+# 			p key
+# 			p i["name"]
+# 			p i["price"]
+# 			p i["margin"]
+# 			p i["quantity_in_hand"]
+# 			p ""
+# 		end
+# 	end
+
+# end
+
+# i = Item.new
+# i.list_all_items
+# i.look_item("tp")
+# i.create_item
